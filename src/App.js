7a06d6ccd3666c7edda6f02f384826029d7d459a
Manavendra07 <manvendra.singh@ens.enterprises>
2024-12-26 01:30:39 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <p className='font-bold text-6xl text-orange-500'>Jai Sree Ram</p>
+    </div>
+  );
+}
+
+export default App;
